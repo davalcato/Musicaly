a4af6ac513f31a6f26b238f523f91ae15bf8d989
davalcato <davalcato@yahoo.com>
2020-08-28 13:08:52 -0700

    first commit

---
+++ Musicaly/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Musicaly
+//
+//  Created by Daval Cato on 8/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Musicaly/MusicalyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicalyApp.swift
+//  Musicaly
+//
+//  Created by Daval Cato on 8/28/20.
+//
+
+import SwiftUI
+
+@main
+struct MusicalyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
